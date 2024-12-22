0765b812ec3122c1391bfdd5d1e15fd77a8a65ac
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-22 14:24:58 -0600

    Make reset button call resetBoard() and clear board

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('text');
+let boardSize = 16;
+
+function createBoard() {
+    
+    // create each row
+    for (i=0; i<boardSize; i++) {
+        
+        //create row items
+        for (j=0; j<boardSize; j++) {
+
+        }
+    }
+}
+
+function resetBoard() {
+    let boardSize = prompt("What size?");
+
+    document.querySelector(".board-container").textContent = "";
+
+    createBoard();
+}
+
+createBoard(boardSize);
+
+document.querySelector("button.reset").addEventListener("click", resetBoard);
