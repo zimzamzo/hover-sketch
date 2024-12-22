b3a619973a18187e261fb93273e32a97e6411897
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-22 14:46:09 -0600

    Implement createBoard()

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ let boardSize = 16;
 
 function createBoard() {
     
-    // create each row
-    for (i=0; i<boardSize; i++) {
-        
-        //create row items
-        for (j=0; j<boardSize; j++) {
+    let boardContainer = document.querySelector(".board-container");
 
+    // populate board
+    for (i=0; i<boardSize; i++) {
+
+        // create row element
+        let boardRow = document.createElement("div");
+        boardRow.classList.add("board-row-container");
+
+        // populate row with items
+        for (j=0; j<boardSize; j++) {
+            let boardItem = document.createElement("div");
+            boardItem.classList.add("board-item");
+            boardRow.appendChild( boardItem );
         }
+        // append the row to the board
+        boardContainer.appendChild( boardRow );
     }
 }
 
 function resetBoard() {
-    let boardSize = prompt("What size?");
+    boardSize = prompt("What size?");
 
     document.querySelector(".board-container").textContent = "";
 
     createBoard();
 }
 
-createBoard(boardSize);
+createBoard();
 
 document.querySelector("button.reset").addEventListener("click", resetBoard);
