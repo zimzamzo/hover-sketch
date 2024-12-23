c102c90e25e42fd5fa147f51147b84680e476c75
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-22 18:54:47 -0600

    Add random color functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let boardSize = 16;
+let randomColorMode = true;
+let gradientMode = false;
 
 function createBoard() {
     
@@ -33,9 +35,29 @@ function resetBoard() {
     createBoard();
 }
 
+// random color generator source from internet, unknown author
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function mouseoverEvent(e) {
     if (e.target.classList.contains("board-item")) {
-        e.target.style.backgroundColor = "black";
+        let color;
+
+        if (randomColorMode) {
+            color = getRandomColor();
+        }
+        else color = "black";
+
+        
+
+        e.target.style.backgroundColor = color;
     }
 }
 
